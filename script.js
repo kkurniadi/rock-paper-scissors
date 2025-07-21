3c22e13915b6845ce9a06fbdb5f4d14b149f4a0f
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2025-07-21 16:17:42 +0800

    Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3) + 1;
+  switch(choice) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
