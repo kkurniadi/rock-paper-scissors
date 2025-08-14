d94ef71b7adcfeb7e7d7987d8acef7e0d81b4f70
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2025-08-14 17:10:21 +0800

    Implement playRound function

--- script.js
+++ script.js
@@ -26,5 +26,27 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-  
+  let outcome = `${humanChoice.toLowerCase()} ${computerChoice}`;
+  switch(outcome) {
+    case "rock scissors":
+    case "scissors paper":
+    case "paper rock":
+      console.log(`You win! ${humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase()} beats ${computerChoice}`);
+      humanScore++;
+      break;
+    case "rock paper":
+    case "scissors rock":
+    case "paper scissors":
+      console.log(`You lose! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.toLowerCase()}`);
+      computerScore++;
+      break;
+    default:
+      console.log("It's a tie!");
+      break;
+  }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
