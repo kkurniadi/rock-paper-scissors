cd6f037f3801d407d24ba3852c9b98caad164d3b
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-02-15 16:36:17 +0800

    Add button query selectors

--- script.js
+++ script.js
@@ -65,3 +65,7 @@ function playGame(rounds) {
 }
 
 // playGame(5);
+
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
