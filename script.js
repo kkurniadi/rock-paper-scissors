4b39546cee828ec3af4d0e53012473d99bff856f
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-02-17 15:23:17 +0800

    Restore and improve endgame logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3) + 1;
-  switch(choice) {
+  switch (choice) {
     case 1:
       return "rock";
     case 2:
@@ -17,7 +17,7 @@ function getComputerChoice() {
 
 function determineWinner(human, computer) {
   const outcome = `${human} ${computer}`;
-  switch(outcome) {
+  switch (outcome) {
     case "rock scissors":
     case "scissors paper":
     case "paper rock":
@@ -31,43 +31,44 @@ function determineWinner(human, computer) {
   }
 }
 
-  let humanScore = 0;
-  let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-  const resultDiv = document.querySelector(".results");
-  const scoresDiv = document.querySelector(".scores");
+const resultDiv = document.querySelector(".results");
+const scoresDiv = document.querySelector(".scores");
 
-  scoresDiv.textContent = "Score is: 0 - 0";
+scoresDiv.textContent = "Score is: 0 - 0";
 
-  function playRound(humanChoice) {
-    const computerChoice = getComputerChoice();
-    const result = determineWinner(humanChoice, computerChoice);
-    if (result == "W") {
-      resultDiv.textContent = `You win! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}`;
-      humanScore++;
-    } else if (result == "L") {
-      resultDiv.textContent = `You lose! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}`;
-      computerScore++;
-    } else {
-      resultDiv.textContent = `It's a tie! You both played ${humanChoice}`;
-    }
-
-    scoresDiv.textContent = `Score is: ${humanScore} - ${computerScore}`;
+function playRound(humanChoice) {
+  const computerChoice = getComputerChoice();
+  const result = determineWinner(humanChoice, computerChoice);
+  if (result == "W") {
+    resultDiv.textContent = `You win! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}`;
+    humanScore++;
+  } else if (result == "L") {
+    resultDiv.textContent = `You lose! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}`;
+    computerScore++;
+  } else {
+    resultDiv.textContent = `It's a tie! You both played ${humanChoice}`;
   }
+  scoresDiv.textContent = `Score is: ${humanScore} - ${computerScore}`;
+}
 
-  /* for (let n = 0; n < rounds; n++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  } */
+/* for (let n = 0; n < rounds; n++) {
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  playRound(humanSelection, computerSelection);
+} */
 
-  /* if (humanScore > computerScore)
+if (humanScore == 5 || computerScore == 5) {
+  if (humanScore > computerScore)
     console.log("Congratulations, you won the game!");
   else if (computerScore > humanScore)
     console.log("The computer won. Better luck next time!");
   else
     console.log("You and the computer are perfectly tied");
- */
+}
+
 // playGame(5);
 
 const rockBtn = document.querySelector(".rock");
